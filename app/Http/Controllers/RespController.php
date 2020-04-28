ef0b15055460f0d97ce39707982cac4d484cdc83
Gabriel Lasaro <gabriel@lasaro.dev>
2020-04-28 13:22:51 -0300

    Generic return treatment

---
+++ app/Http/Controllers/RespController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RespController extends Controller {
+
+    public static function affected($result) {
+        if ($result==0) {
+            return response([
+                'status' => false,
+                'message' => 'Nenhuma informação encontrada ou não havia nada a ser feito.'
+            ], 404);
+        } elseif ($result<0) {
+            return response([
+                'status' => false,
+                'message' => 'Erro desconhecido.'
+            ], 500);
+        }
+
+        return array(
+            'status'=>true,
+            'message'=> 'Sucesso.'
+        );
+    }
+
+    public static function returnId($result) {
+        if ($result<0) {
+            return response([
+                'status' => false,
+                'message' => 'Erro desconhecido.'
+            ], 500);
+        }
+        
+        return [
+            'status' => true,
+            'message' => 'Sucesso.',
+            'id' => $result
+        ];
+    }
+
+    public static function returnData($result) {
+        if (!$result[0]) {
+            return response([
+                'status' => false,
+                'message' => 'Erro desconhecido ao obter dados.'
+            ], 500);
+        }
+
+        if (sizeof($result[1])==0) {
+            return response([
+                'status' => false,
+                'message' => 'Nada foi encontrada.'
+            ], 404);
+        }
+        
+        return [
+            'status' => true,
+            'message' => 'Sucesso.',
+            'data' => $result[1]
+        ];
+    }
+}
